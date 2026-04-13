e3b58350998670128a816f69cf24ac7131588392
lmoraity21 <lmoraity@scentia.com.au>
2026-04-13 11:04:14 +1000

    Added basename to Browser router

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import "@fontsource/poppins/700.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/achw-browser-support-guide">
       <ScrollToTop />
       <App />
       <Footer />
