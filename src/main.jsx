b1cfd6a0a6e4a757bbd616108edc68dfb62c07b6
lmoraity21 <lmoraity@scentia.com.au>
2026-04-13 11:47:33 +1000

    Changed out browser router for Hash

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import Footer from "./components/Footer.jsx";
 import ScrollToTop from "./components/ScrollToTop.jsx";
 
@@ -14,10 +14,10 @@ import "@fontsource/poppins/700.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/achw-browser-support-guide">
+    <HashRouter>
       <ScrollToTop />
       <App />
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
